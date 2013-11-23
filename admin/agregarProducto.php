2423ec8875bdae2e69e3aae68918a02a3bfd6e5a
damgel <damgel@outlook.com>
2013-11-23 13:30:24 -0600

    ruta de almacenamiento de las imagenes arreglada.

--- admin/agregarProducto.php
+++ admin/agregarProducto.php
@@ -3,8 +3,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Agregar Producto</title>
-        <link href="assets/css/main.css" rel="stylesheet" type="text/css" />
-        <script src="assets/js/jquery-v1.10.2.js"></script>
+        <link href="../assets/css/main.css" rel="stylesheet" type="text/css" />
+        <script src="../assets/js/jquery-v1.10.2.js"></script>
         <script src="http://malsup.github.com/jquery.form.js"></script>
         <style>
             form { display: block; margin: 20px auto;  border-radius: 10px; padding: 15px }
@@ -24,13 +24,13 @@
         </style>
     </head>
     <body>
-        <?php include_once 'Includes/header.php'; ?>
+        <?php include_once '../Includes/header.php'; ?>
         <div class="contenedor">
             <?php
-            include_once 'clases/db_connect.php';
+            include_once '../clases/db_connect.php';
             if (isset($_POST['submitted'])) {
                 /* ------------------------------------BEGIN FILE UPLOADER-------------------------------------------- */
-                $output_dir = "Images/productos/";
+                $output_dir = "../Images/productos/";
                 $name_ok;
                 if (isset($_FILES["myfile"])) {
                     //Filter the file types , if you want.
@@ -47,11 +47,11 @@
                 foreach ($_POST AS $key => $value) {
                     $_POST[$key] = $value;
                 }
-                $sql = "INSERT INTO `producto` (`nombre_p` ,  `descripcion_p` ,  `categoria_p` ,   `marca_p` , `precio_p` ,  `costo_p` ,  `activo_p` ,  `imagen_p`,  `fecha_p`   ) VALUES( '{$_POST['nombre_p']}' ,  '{$_POST['descripcion_p']}' ,  '{$_POST['categoria_p']}' , '{$_POST['marca_p']}' ,  '{$_POST['precio_p']}' , '{$_POST['costo_p']}' ,  '{$_POST['activo_p']}' , '$name_ok' ,  now())";
+                $sql = "INSERT INTO `producto` (`nombre_p` ,  `descripcion_p` ,  `categoria_p` ,   `marca_p`  ,   `tamanio_p`, `color_p` , `precio_p` ,  `costo_p` ,  `activo_p` ,  `imagen_p`,  `fecha_p`   ) VALUES( '{$_POST['nombre_p']}' ,  '{$_POST['descripcion_p']}' ,  '{$_POST['categoria_p']}' , '{$_POST['marca_p']}' , '{$_POST['tamanio_p']}' , '{$_POST['color_p']}',  '{$_POST['precio_p']}' , '{$_POST['costo_p']}' ,  '{$_POST['activo_p']}' , '$name_ok' ,  now())";
                 mysql_query($sql) or die(mysql_error());
                 echo "<script>alert('$name_ok')</script>";
                 echo "Registro Agregado.<br />";
-                echo "<a href='verProductos.php'>Regresar</a>";
+                echo "<a href='administrarProductos.php'>Regresar</a>";
             }
             ?>
 
@@ -60,8 +60,10 @@
                     <div><input type="file" name="myfile"></div><br>
                     <div><b>Nombre del Producto:</b><br /><input type='text' name='nombre_p' required/></div> 
                     <div><b>Descripcion del Producto:</b><br /><input type='text' name='descripcion_p' required/></div> 
-                    <div><b>Categoria:</b><br><select name="categoria_p"><option>-seleccione-</option><?php include_once 'clases/listas/categorias.php'; ?></select></div>
+                    <div><b>Categoria:</b><br><select name="categoria_p"><option>-seleccione-</option><?php include_once '../clases/listas/categorias.php'; ?></select></div>
                     <div><b>Marca:</b><br /><input type='text' name='marca_p' required/></div> 
+                    <div><b>Talla:</b><br><select name="tamanio_p"><option>-seleccione-</option><?php include_once '../clases/listas/tallas.php'; ?></select></div>
+                    <div><b>Color:</b><br><select name="color_p"><option>-seleccione-</option><?php include_once '../clases/listas/colores.php'; ?></select></div>
                     <div><b>Precio de Venta:</b><br /><input type='text' name='precio_p' required/></div> 
                     <div><b>Precio de Costo:</b><br /><input type='text' name='costo_p' required/></div> 
                     <div><select name="activo_p">
