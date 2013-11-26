286ae6c3c133c1c0cbe17ee2f08fdc8628aeece0
gaby <monteshernandez6@hotmail.com>
2013-11-26 00:02:29 -0600

    edit

---
+++ cliente.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `cliente` (
+  `idcliente` int(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(120) COLLATE latin1_general_ci DEFAULT NULL,
+  `apellido` varchar(120) COLLATE latin1_general_ci DEFAULT NULL,
+  `usuario` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
+  `contrasenia` varchar(250) COLLATE latin1_general_ci DEFAULT NULL,
+  `fecha_nac` date DEFAULT NULL,
+  `correo` varchar(250) COLLATE latin1_general_ci DEFAULT NULL,
+  `tel` varchar(20) COLLATE latin1_general_ci DEFAULT NULL,
+  PRIMARY KEY (`idcliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
--- registrocliente.php
+++ registrocliente.php
@@ -13,30 +13,76 @@
 
     </head>
     <style>
+
     </style>
     <body>
 
 
 <? 
-include(''); 
-if (isset($_POST['submitted'])) { 
+include('../clases/db_connect.php'); 
+if (isset($_POST['Enviar'])) { 
 foreach($_POST AS $key => $value) { $_POST[$key] = mysql_real_escape_string($value); } 
 $sql = "INSERT INTO `cliente` (`nombre` ,  `apellido` ,  `usuario` ,  `contrasenia` ,  `fecha_nac` ,  `correo` ,  `tel`  ) VALUES(  '{$_POST['nombre']}' ,  '{$_POST['apellido']}' ,  '{$_POST['usuario']}' ,  '{$_POST['contrasenia']}' ,  '{$_POST['fecha_nac']}' ,  '{$_POST['correo']}' ,  '{$_POST['tel']}'  ) "; 
 mysql_query($sql) or die(mysql_error()); 
-echo "Added row.<br />"; 
-echo "<a href='list.php'>Back To Listing</a>"; 
 } 
 ?>
-<div>
-<form action='' method='POST'> 
-<p><b>Nombre:</b><br /><input type='text' name='nombre'/></p> 
-<p><b>Apellido:</b><br /><input type='text' name='apellido'/></p> 
-<p><b>Usuario:</b><br /><input type='text' name='usuario'/> </p>
-<p><b>Contrasenia:</b><br /><input type='text' name='contrasenia'/> </p>
-<p><b>Fecha Nacimiento:</b><br /><input type='text' name='fecha_nac'/> </p>
-<p><b>Correo:</b><br /><input type='text' name='correo'/> </p>
-<p><b>Telefono:</b><br /><input type='text' name='tel'/> </p>
-<p><input type='submit' value='Enviar' /><input type='hidden' value='1' name='submitted' /> </p>
+<div  class="col-md-8 col-md-offset-2">
+
+<form action='' method='POST' class="form-horizontal" role="form"> 
+<br>
+<div class="form-group">
+<label class="col-sm-2 control-label">Nombre</label>
+<div class="col-sm-6">
+<input type='text' name='nombre' class="form-control"/>
+</div>
+</div>
+
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Apellido</label>
+<div class="col-sm-6">
+<input type='text' name='apellido' class="form-control"/> 
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Usuario</label>
+<div class="col-sm-6">
+<input type='text' name='usuario' class="form-control"/> 
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Contrasenia</label>
+<div class="col-sm-6">
+<input type='text' name='contrasenia' class="form-control"/>
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Fecha Nacimiento</label>
+<div class="col-sm-6">
+<input type='text' name='fecha_nac' class="form-control"/>
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Correo</label>
+<div class="col-sm-6">
+<input type='text' name='correo' class="form-control"/>
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Telefono</label>
+<div class="col-sm-6">
+<input type='text' name='tel' class="form-control"/>
+</div>
+</div>
+
+
+<p align="center"><input type='submit' value='Enviar' class="btn btn-primary btn-lg" />
+<input type='hidden' value='1' name='Enviar' /> </p>
 </form> 
 
 </div>
