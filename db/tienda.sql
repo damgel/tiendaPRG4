bb008f61c89e5f4cc77a41be657cd8b18b37fda3
damgel <damgel@outlook.com>
2013-11-28 03:41:51 -0600

    ultimos cambios en la db

--- db/tienda.sql
+++ db/tienda.sql
@@ -33,7 +33,7 @@ CREATE TABLE `carrito` (
   `cantidad` float DEFAULT '0',
   `subtotal` double DEFAULT '0',
   PRIMARY KEY (`idcarrito`)
-) ENGINE=InnoDB AUTO_INCREMENT=149 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=162 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,6 @@ CREATE TABLE `carrito` (
 
 LOCK TABLES `carrito` WRITE;
 /*!40000 ALTER TABLE `carrito` DISABLE KEYS */;
-INSERT INTO `carrito` VALUES (147,24,1,'android daBeast2',250,10,2500),(148,22,1,'android daBeast',250,6,1500);
 /*!40000 ALTER TABLE `carrito` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,7 +99,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'jose','guillen','damgel','e703908953979aba5049ec2e83f4e104282abe84','1992-11-22','deakill@hotmail.com','79263016',NULL),(3,'jose ariel','rivas','jrivas','e703908953979aba5049ec2e83f4e104282abe84','1994-07-14','jariel@yahoo.com','79263016','5296e25d6c5df');
+INSERT INTO `cliente` VALUES (1,'jose','guillen','damgel','e703908953979aba5049ec2e83f4e104282abe84','1992-11-22','deakill@hotmail.com','79263016','5297084c3e6e1'),(3,'jose ariel','rivas','jrivas','e703908953979aba5049ec2e83f4e104282abe84','1994-07-14','jariel@yahoo.com','79263016','5296e25d6c5df');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +146,7 @@ CREATE TABLE `compra` (
   `total` float DEFAULT NULL,
   PRIMARY KEY (`idcompra`),
   UNIQUE KEY `idcompra_UNIQUE` (`idcompra`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,7 +155,7 @@ CREATE TABLE `compra` (
 
 LOCK TABLES `compra` WRITE;
 /*!40000 ALTER TABLE `compra` DISABLE KEYS */;
-INSERT INTO `compra` VALUES (77,'5296e25d6c5df',3,'2013-11-28 00:27:41',5,0),(78,'5296e25d6c5df',3,'2013-11-28 00:30:41',5,0),(79,'5296e25d6c5df',3,'2013-11-28 01:50:19',1,0);
+INSERT INTO `compra` VALUES (85,'5297084c3e6e1',1,'2013-11-28 03:39:59',5,800);
 /*!40000 ALTER TABLE `compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +174,7 @@ CREATE TABLE `detalles_compra` (
   `fecha` datetime DEFAULT NULL,
   PRIMARY KEY (`idcompra_d`),
   UNIQUE KEY `idcompra_d` (`idcompra_d`)
-) ENGINE=InnoDB AUTO_INCREMENT=199 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=214 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,7 +183,7 @@ CREATE TABLE `detalles_compra` (
 
 LOCK TABLES `detalles_compra` WRITE;
 /*!40000 ALTER TABLE `detalles_compra` DISABLE KEYS */;
-INSERT INTO `detalles_compra` VALUES (192,3,'zapatos','5296e25d6c5df','2013-11-28 00:27:41'),(193,3,'BLACKBERRY','5296e25d6c5df','2013-11-28 00:27:41'),(194,3,'sombrero playa','5296e25d6c5df','2013-11-28 00:27:41'),(195,3,'zapatos','5296e25d6c5df','2013-11-28 00:30:41'),(196,3,'BLACKBERRY','5296e25d6c5df','2013-11-28 00:30:41'),(197,3,'sombrero playa','5296e25d6c5df','2013-11-28 00:30:41'),(198,3,'BLACKBERRY','5296e25d6c5df','2013-11-28 01:50:20');
+INSERT INTO `detalles_compra` VALUES (209,1,'android daBeast2','5297084c3e6e1','2013-11-28 03:39:59'),(210,1,'0000 TELEFONO','5297084c3e6e1','2013-11-28 03:39:59'),(211,1,'BLACKBERRY','5297084c3e6e1','2013-11-28 03:39:59'),(212,1,'BLACKBERRY','5297084c3e6e1','2013-11-28 03:39:59'),(213,1,'TELEFONO CLASICO','5297084c3e6e1','2013-11-28 03:40:00');
 /*!40000 ALTER TABLE `detalles_compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -220,7 +219,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` VALUES (3,'Images/productos/zapato1.jpg','zapatos','calidad mundial garantizada','-seleccione-',36.17,'S',0,'2013-11-28','nike',15,'S','BLANCO'),(13,'Images/productos/527eb966d64ba102506.jpg','camisa sport','testing testing testing ','Camisas',17.25,'N',500,'2013-11-28','adidas',10,'S','AZUL'),(14,'Images/productos/5280096c4410611-(70)b.jpg','New Jacket','three pieces jacket','Camisas',120.99,'S',500,'2013-11-28','nike',10,'M','NEGRO'),(17,'Images/productos/5287c0fbec473Telefono.jpg','TELEFONO CLASICO','BRAND NEW PRODUCT','Electronicos',125,'S',500,'2013-11-28','CLASSIC',10,'M','ROJO'),(18,'Images/productos/5287c130cd9432566829399_86e0600664_m_thumb.jpg','producto de prueba','BRAND NEW PRODUCT','Electronicos',54.0514,'S',500,'2013-11-23','Apple',10,'S','ROSADO'),(19,'Images/productos/5287c2e416fe7htc-desire_0.jpg','0000 TELEFONO','BRAND NEW TELEFONO','Electronicos',125,'S',500,'2013-11-17','HTC',10,'XE','AMARILLO'),(20,'Images/productos/5287c3c576f58ADS.JPG','BLACKBERRY','BLACKBERRY SD152-01','Electronicos',150,'S',497,'2013-11-17','BB',10,'XM','BLANCO'),(21,'Images/productos/528b919259dd5descarga.jpg','sombrero playa','palma','Sombreros',28,'S',498,'2013-11-19','ninguna',8,'XS','NEGRO'),(22,'Images/productos/3570-android.jpg','android daBeast','KITKAT VERSION','Electronicos',250,'S',500,'2013-11-28','google',150,'S','BLANCO'),(24,'../Images/productos/5291020a85e4bandroid_eating_apple.png','android daBeast2','KITKAT VERSION2','Electronicos',250,'S',501,'2013-11-28','google',100,'M','CELESTE');
+INSERT INTO `producto` VALUES (3,'Images/productos/zapato1.jpg','zapatos','calidad mundial garantizada','-seleccione-',36.17,'S',0,'2013-11-28','nike',15,'S','BLANCO'),(13,'Images/productos/527eb966d64ba102506.jpg','camisa sport','testing testing testing ','Camisas',17.25,'N',500,'2013-11-28','adidas',10,'S','AZUL'),(14,'Images/productos/5280096c4410611-(70)b.jpg','New Jacket','three pieces jacket','Camisas',120.99,'S',500,'2013-11-28','nike',10,'M','NEGRO'),(17,'Images/productos/5287c0fbec473Telefono.jpg','TELEFONO CLASICO','BRAND NEW PRODUCT','Electronicos',125,'S',499,'2013-11-28','CLASSIC',10,'M','ROJO'),(18,'Images/productos/5287c130cd9432566829399_86e0600664_m_thumb.jpg','producto de prueba','BRAND NEW PRODUCT','Electronicos',54.0514,'S',500,'2013-11-23','Apple',10,'S','ROSADO'),(19,'Images/productos/5287c2e416fe7htc-desire_0.jpg','0000 TELEFONO','BRAND NEW TELEFONO','Electronicos',125,'S',498,'2013-11-17','HTC',10,'XE','AMARILLO'),(20,'Images/productos/5287c3c576f58ADS.JPG','BLACKBERRY','BLACKBERRY SD152-01','Electronicos',150,'S',482,'2013-11-17','BB',10,'XM','BLANCO'),(21,'Images/productos/528b919259dd5descarga.jpg','sombrero playa','palma','Sombreros',28,'S',498,'2013-11-19','ninguna',8,'XS','NEGRO'),(22,'Images/productos/3570-android.jpg','android daBeast','KITKAT VERSION','Electronicos',250,'S',489,'2013-11-28','google',150,'S','BLANCO'),(24,'../Images/productos/5291020a85e4bandroid_eating_apple.png','android daBeast2','KITKAT VERSION2','Electronicos',250,'S',488,'2013-11-28','google',100,'M','CELESTE');
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -289,4 +288,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-11-28  2:44:58
+-- Dump completed on 2013-11-28  3:41:26
